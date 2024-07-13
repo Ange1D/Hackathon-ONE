50647ba3b8010300e0a2a2d03897f2072781c62b
Ange1D <47231898+Ange1D@users.noreply.github.com>
2024-07-13 17:46:00 -0600

    Create videogame model and dto

---
+++ api/src/main/java/com/hackathon/api/model/entity/Videogame.java
@@ -0,0 +1,25 @@
+package com.hackathon.api.model.entity;
+
+import jakarta.persistence.*;
+import jdk.jshell.Snippet;
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name= "video-games")
+public class Videogame implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String status;
+
+}
