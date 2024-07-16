8a1505596cba3ec552e1d8e84dcc765ad02b9eeb
Ange1D <47231898+Ange1D@users.noreply.github.com>
2024-07-16 14:14:19 -0600

    Update SecurityConfig.java

--- api/src/main/java/com/hackathon/api/Security/SecurityConfig.java
+++ api/src/main/java/com/hackathon/api/Security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/hello").permitAll()
                         .anyRequest().authenticated()
