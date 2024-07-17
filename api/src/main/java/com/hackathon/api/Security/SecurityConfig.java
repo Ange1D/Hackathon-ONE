6acde227267bc3bd1ba9c9c51903c4b71958ad62
Ange1D <47231898+Ange1D@users.noreply.github.com>
2024-07-17 14:29:39 -0600

    Create /login

--- api/src/main/java/com/hackathon/api/Security/SecurityConfig.java
+++ api/src/main/java/com/hackathon/api/Security/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.hackathon.api.Security.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
                         .requestMatchers("/api/v1/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/hello").permitAll()
