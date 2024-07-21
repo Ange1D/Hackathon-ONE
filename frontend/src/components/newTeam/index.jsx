77e710fde8f08dc79fa1d00262e63248fe228d7e
Rosnye <davd326at@gmail.com>
2024-07-20 19:27:51 -0600

    Add new Team content and styles

--- frontend/src/components/newTeam/index.jsx
+++ frontend/src/components/newTeam/index.jsx
@@ -1,9 +1,23 @@
-
+import "./newTeam.css"
 
 const NewTeam = () =>{
     return(
-        <div>
-            Team Team
+        <div className="yourTeam">
+            <div className="yourTeamTitle">
+            <img src="./images/splashart-lol2.png" alt="Splashart-lol2" />
+                <h2>YOUR TEAM</h2>
+            </div>
+            <div className="yourTeamInfo">
+                <h3>NAME</h3>
+                <input type="text" required/>
+                <h3>GAME</h3>
+                <input type="text" required/>
+                <h3>COUNTRY</h3>
+                <input type="text" required/>
+                <h3>INITIALS</h3>
+                <input type="text" required/>
+                <button type="submit">CREATE</button>
+            </div>
         </div>
     )
 }
