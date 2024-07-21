861f407d939c2c33a70a1e7af70dc3493df39bc6
Rosnye <davd326at@gmail.com>
2024-07-20 19:15:47 -0600

    Add login content and styles

--- frontend/src/components/login/index.jsx
+++ frontend/src/components/login/index.jsx
@@ -2,10 +2,19 @@ import "./login.css"
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
-            <h3>Email</h3>
-            <input type="text"/>
+        <div className="login">
+            <div className="loginTitle">
+            <img src="./images/splashart-lol1.png" alt="Splashart-lol1" />
+                <h2>LOGIN</h2>
+            </div>
+            <div className="loginCred">
+                <h3>EMAIL</h3>
+                <input type="email" required/>
+                <h3>PASSWORD</h3>
+                <input type="password"/>
+                <label htmlFor=""><input type="checkbox" className="check"/> Remember Me</label>
+                <button>LOGIN</button>
+            </div>
         </div>
     )
 }
