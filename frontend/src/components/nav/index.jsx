ad43a1e1ac5c3010e85eb81298984e7d70dbe856
Diana Castaño <dianaczapata@gmail.com>
2024-07-23 21:57:44 -0500

    add desktop styles

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,4 +1,5 @@
 import "./nav.css"
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return(
@@ -11,9 +12,14 @@ const Nav = () => {
                 <img src="./images/leeroy-icon.png" alt="leeroy-icon" />
                 <h2>Leeroy.GG</h2>
             </a>
-            <div>
-
-            </div>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/profile'>Your profile</Link></li>
+                <li><Link to='/teamCreated'>Your teams</Link></li>
+                <li><Link to='/teamFinder'>Teams finder</Link></li>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/register'>Sign up</Link></li>
+            </ul>
         </nav>
     )
 }
